eee1217e9dcfb546ef8fb266d51a82371af0ead2
yuelang <1255280233@qq.com>
2021-04-25 22:53:02 +0800

    Go进阶训练营第二周作业

---
+++ week2.go
@@ -0,0 +1,14 @@
+package main
+
+/*
+作业问题：
+
+我们在数据库操作的时候，比如 dao 层中当遇到一个 sql.ErrNoRows 的时候，
+是否应该 Wrap 这个 error，抛给上层。为什么，应该怎么做请写出代码？
+
+
+答：
+应该将 sql.ErrNoRows 往上抛，因为在与数据库操作的时候
+会调用到第三方库或者是标准库，这个时候需要考虑使用errors.Wrap 或 errors.Wrapf
+保存堆栈信息，然后抛给上层调用方。
+*/
